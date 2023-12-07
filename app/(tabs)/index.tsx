543d42e52b123f51613ad9ea0054a32a80c6e281
thefappybird <alex.banaag1@gmail.com>
2023-12-07 22:48:46 +0800

    Hackathon Project First commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,53 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-
+import { useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import { COLORS, icons, images, SIZES } from "../../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  Welcome,
+} from "../../components";
+import { useRouter } from "expo-router";
 export default function TabOneScreen() {
+  const router = useRouter()
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.mainContainer}>
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={()=>{
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`)
+              }
+            }}
+          />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: SIZES.medium,
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: COLORS.lightWhite,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
+    backgroundColor: "black",
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
